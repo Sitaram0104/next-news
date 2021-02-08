59090c22f2329ebaf61aefb0287521725394572a
Sitaram <meenasitu0104@gmail.com>
2021-02-08 18:25:39 +0530

    Init

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import { Toolbar } from "../components/toolbar";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Toolbar />
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
